1db0c4b591c9f988378e8c82ddb58312bfbdd7ab
Pablo Zaidenvoren <pabloz@lagash.com>
2016-06-30 22:53:54 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # html-localizer
 Static html files localization middleware for Express
+
+## How to use
+
+Inside your app.js express file, just before adding the static files middleware, you should add the following lines:
+
+```
+var loclib = require('html-localizer');
+
+var staticFilesDir = path.join(__dirname, 'public');
+var localizer = new loclib.default(staticFilesDir);
+
+// let's add support for english (default) and spanish languages
+loc.loadLanguage('en', path.join(__dirname, 'locale.en'), true);
+loc.loadLanguage('es', path.join(__dirname, 'locale.es'));
+app.use(loc.middleware());
+
+// now, mount the actual static files handler on both language paths
+app.use('/en', express.static(staticFilesDir));
+app.use('/es', express.static(staticFilesDir));
+
+```
---
+++ index.js
@@ -0,0 +1,86 @@
+var fs = require("fs");
+var endOfLine = require('os').EOL;
+var path = require('path');
+
+let Localizer = class Localizer {
+    constructor(staticFilesPath) {
+        this._staticFilesPath = staticFilesPath;
+        this._lang = {};
+        this._defaultLang
+    }
+
+    loadLanguage(lang, file, isDefault) {
+        this._lang[lang] = {};
+
+        var content = fs.readFileSync(file).toString();
+
+        for (var line of content.split(endOfLine)) {
+            var equal = line.indexOf("=");
+            var id = line.substring(0, equal);
+            var value = line.substring(equal + 1);
+
+            this._lang[lang][id] = value;
+        }
+
+        if (isDefault || this._defaultLang == undefined) {
+            this._defaultLang = lang;
+        }
+    }
+
+    middleware() {
+        var realThis = this;
+        return function (req, res, next) {
+            var currentLang = realThis.getLang(req.url);
+            var reqPath = realThis.removeLang(req.url);
+
+            var filePath = path.join(realThis._staticFilesPath, reqPath);
+
+            if (filePath.endsWith("/")) {
+                filePath += "index.html";
+            }
+
+            if (!filePath.endsWith(".html")) {
+                next();
+                return;
+            }
+
+            if (!fs.existsSync(filePath)) {
+                next();
+                return;
+            }
+
+            var content = fs.readFileSync(filePath).toString();
+            for (var key in realThis._lang[currentLang]) {
+                var val = realThis._lang[currentLang][key];
+                content = content.replace(key, val);
+            }
+
+            res.send(content);
+        }
+    }
+
+    removeLang(path) {
+        for (var lang in this._lang) {
+            var start = "/" + lang;
+            if (path.startsWith(start)) {
+                return path.substr(start.length);
+            }
+        }
+
+        return path;
+    }
+
+    getLang(path) {
+        for (var lang in this._lang) {
+            var start = "/" + lang;
+            if (path.startsWith(start)) {
+                return lang;
+            }
+        }
+
+        return this._defaultLang;
+    }
+}
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Localizer;
